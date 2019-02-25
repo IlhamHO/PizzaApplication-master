d8b694685d31fc1994bb00b57b68059ba05e23fa
mobapp10 <mobapp10@student.ehb.be>
2019-02-25 14:47:59 +0100

    toppings checking annuleren- activity_main.xml

--- app/src/main/java/com/example/pizzaapplication/MainActivity.java
+++ app/src/main/java/com/example/pizzaapplication/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     private CoordinatorLayout myLayout;
 
     private Button btnFormatOrder;
-    private String [] listFormat;
+    private String[] listFormat;
     private TextView formatSelected;
     private String chooseItem;
 
@@ -28,19 +28,19 @@ public class MainActivity extends AppCompatActivity {
     private String[] listItems;
     private TextView itemSelected;
     private boolean[] checkedItems;
-    private ArrayList<Integer> mUserItems = new ArrayList<>();
+    private ArrayList<String> mUserItems = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        myLayout = (CoordinatorLayout)findViewById(R.id.myLayout);
-        btnOrder= (Button)findViewById(R.id.btn_order);
-        btnFormatOrder = (Button)findViewById(R.id.btn_formatOrder);
-        btnToppingsOrder = (Button)findViewById(R.id.btn_toppingsOrder);
-        itemSelected = (TextView)findViewById(R.id.tv_order);
-        formatSelected =(TextView)findViewById(R.id.tv_format);
+        myLayout = (CoordinatorLayout) findViewById(R.id.myLayout);
+        btnOrder = (Button) findViewById(R.id.btn_order);
+        btnFormatOrder = (Button) findViewById(R.id.btn_formatOrder);
+        btnToppingsOrder = (Button) findViewById(R.id.btn_toppingsOrder);
+        itemSelected = (TextView) findViewById(R.id.tv_order);
+        formatSelected = (TextView) findViewById(R.id.tv_format);
 
         listItems = getResources().getStringArray(R.array.shopping_item);
         checkedItems = new boolean[listItems.length];
@@ -55,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
                 builder.setSingleChoiceItems(listFormat, -1, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        chooseItem= listFormat[which];
+                        chooseItem = listFormat[which];
                     }
                 });
                 builder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
@@ -84,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-       btnToppingsOrder.setOnClickListener(new View.OnClickListener() {
+        btnToppingsOrder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
@@ -92,65 +92,64 @@ public class MainActivity extends AppCompatActivity {
                 mBuilder.setMultiChoiceItems(listItems, checkedItems, new DialogInterface.OnMultiChoiceClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int position, boolean isChecked) {
-                        if(isChecked){
-                            if(!mUserItems.contains(position)){
-                                mUserItems.add(position);
-                            } else if (mUserItems.contains(position)){
-                                mUserItems.remove(position);
-                            }
-                                           }
+                        if (isChecked) {
+                                mUserItems.add(listItems[position]);
+
+                        }else {
+                            mUserItems.remove(listItems[position]);
+                        }
 
                     }
                 });
-            mBuilder.setCancelable(false);
-            mBuilder.setPositiveButton(R.string.ok_label, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int which) {
-                    String item = "";
-                    for(int i =0; i < mUserItems.size(); i++ ) {
-                        item = item + listItems[mUserItems.get(i)];
-                        if(i != mUserItems.size() -1){
-                            item = item + ",";
+                mBuilder.setCancelable(false);
+                mBuilder.setPositiveButton(R.string.ok_label, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        String item = "";
+                        for (int i = 0; i < mUserItems.size(); i++) {
+                            item = item + mUserItems.get(i);
+                            if (i != mUserItems.size() - 1) {
+                                item = item + ",";
+                            }
+                        }
+                        itemSelected.setText(item);
+                    }
+                });
+                mBuilder.setNegativeButton(R.string.dismiss_label, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                mBuilder.setNeutralButton(R.string.clear_all_label, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        for (int i = 0; i < checkedItems.length; i++) {
+                            checkedItems[i] = false;
+                            mUserItems.clear();
+                            itemSelected.setText("");
                         }
                     }
-                    itemSelected.setText(item);
-                    }
-            });
-            mBuilder.setNegativeButton(R.string.dismiss_label, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int which) {
-                    dialogInterface.dismiss();
-                }
-            });
-            mBuilder.setNeutralButton(R.string.clear_all_label, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int which) {
-                for(int i = 0; i< checkedItems.length; i++){
-                    checkedItems[i]= false;
-                    mUserItems.clear();
-                    itemSelected.setText("");
-                }
-                }
-            });
-            AlertDialog mDialog = mBuilder.create();
-            mDialog.show();
+                });
+                AlertDialog mDialog = mBuilder.create();
+                mDialog.show();
             }
         });
 
 
-       btnOrder.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-Snackbar sb;
-sb=Snackbar.make(myLayout,"Order within",Snackbar.LENGTH_LONG);
-sb.setAction("Cancel", new View.OnClickListener() {
-    @Override
-    public void onClick(View v) {
+        btnOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar sb;
+                sb = Snackbar.make(myLayout, "Order within", Snackbar.LENGTH_LONG);
+                sb.setAction("Cancel", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
 
-    }
-});
-               sb.show();
-           }
-       });
+                    }
+                });
+                sb.show();
+            }
+        });
     }
 }
